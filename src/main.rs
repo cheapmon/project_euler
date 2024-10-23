c3a09ad4356a8fdbeedf7e336bd7f197644a1894
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2024-10-23 15:11:37 +0200

    Problem 14

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,8 @@ fn main() {
     // println!("Problem 10: {}", problem_10());
     // println!("Problem 11: {}", problem_11());
     // println!("Problem 12: {}", problem_12());
-    println!("Problem 13: {}", problem_13());
+    // println!("Problem 13: {}", problem_13());
+    println!("Problem 14: {}", problem_14());
 }
 
 // Problem 1
@@ -339,4 +340,44 @@ fn problem_13() -> u64 {
         .map(|line| line.trim().parse::<BigInt>().unwrap())
         .sum();
     sum.to_string()[0..10].parse::<u64>().unwrap()
+}
+
+// Problem 14
+fn problem_14() -> u64 {
+    let mut longest_chain = 1;
+    let mut number_which_produces_longest_chain = 1;
+
+    let mut cache: HashMap<u64, u64> = HashMap::new();
+    cache.insert(1, 1);
+
+    fn collatz(i: u64) -> u64 {
+        if i % 2 == 0 {
+            i / 2
+        } else {
+            3 * i + 1
+        }
+    }
+
+    fn compute_length_of_chain(i: u64, cache: &mut HashMap<u64, u64>) -> u64 {
+        if cache.contains_key(&i) {
+            cache[&i]
+        } else {
+            let next = collatz(i);
+            let length_of_chain = compute_length_of_chain(next, cache) + 1;
+
+            cache.insert(i, length_of_chain);
+            length_of_chain
+        }
+    }
+
+    for i in 2..1_000_000 {
+        let length_of_chain = compute_length_of_chain(i, &mut cache);
+
+        if (length_of_chain > longest_chain) {
+            longest_chain = length_of_chain;
+            number_which_produces_longest_chain = i;
+        }
+    }
+
+    number_which_produces_longest_chain
 }
